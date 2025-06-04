a38f4c524d5cb09670e9448d4fc9ae70a2999bf5
Cristian Prieto <pcristiandev@gmail.com>
2025-06-04 13:40:56 -0300

    Agregue la carpeta dominio junto con las clases

---
+++ dominio/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Articulo
+    {
+        public int Id { get; set; }
+        public int CodigoDeArticulo { get; set; }
+        public int Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca MarcaDelArticulo { get; set; }
+        public Categoria CategoriaDelArticulo { get; set; }
+        public string UrlImagen { get; set; }
+        public double Precio { get; set; }
+
+
+    }
+}

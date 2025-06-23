8d9c3d074c7efa183a22f88435a9a9ea307bfaf7
Cristian Prieto <pcristiandev@gmail.com>
2025-06-23 15:37:31 -0300

    Coloque Urls validas a las imagenes

--- negocio/scrip_db/CATALOGO_DB.sql
+++ negocio/scrip_db/CATALOGO_DB.sql
@@ -71,9 +71,9 @@ SET ANSI_PADDING OFF
 GO
 insert into MARCAS values ('Samsung'), ('Apple'), ('Sony'), ('Huawei'), ('Motorola')
 insert into CATEGORIAS values ('Celulares'),('Televisores'), ('Media'), ('Audio')
-insert into ARTICULOS values ('S01', 'Galaxy S10', 'Una canoa cara', 1, 1, 'https://images.samsung.com/is/image/samsung/assets/ar/p6_gro2/p6_initial_mktpd/smartphones/galaxy-s10/specs/galaxy-s10-plus_specs_design_colors_prism_black.jpg?$163_346_PNG$', 69.999),
-('M03', 'Moto G Play 7ma Gen', 'Ya siete de estos?', 5, 1, 'https://www.motorola.cl/arquivos/moto-g7-play-img-product.png?v=636862863804700000', 15699),
+insert into ARTICULOS values ('S01', 'Galaxy S10', 'Una canoa cara', 1, 1, 'https://drive.google.com/uc?export=view&id=1zPr0rfMNr49qVg9CQ8gNUN27YSbJQPBl', 69.999),
+('M03', 'Moto G Play 7ma Gen', 'Ya siete de estos?', 5, 1, 'https://drive.google.com/uc?export=view&id=1xvSeja6uv5x1a5MDskesY8iFF3o5O7eK', 15699),
 ('S99', 'Play 4', 'Ya no se cuantas versiones hay', 3, 3, 'sin_imagen_para_que_de_error....muejeje', 35000),
-('S56', 'Bravia 55', 'Alta tele', 3, 2, 'https://intercompras.com/product_thumb_keepratio_2.php?img=images/product/SONY_KDL-55W950A.jpg&w=650&h=450', 49500),
-('A23', 'Apple TV', 'lindo loro', 2, 3, 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/rfb-apple-tv-4k?wid=1144&hei=1144&fmt=jpeg&qlt=80&.v=1513897159574', 7850)
+('S56', 'Bravia 55', 'Alta tele', 3, 2, 'https://drive.google.com/uc?export=view&id=1wXnx0mKQBzE0qgCNnTvMkMGaihPHoJ5O', 49500),
+('A23', 'Apple TV', 'lindo loro', 2, 3, 'https://drive.google.com/uc?export=view&id=1VpWreXeW6lXiJMvxXgcoKGceZva7NPf5', 7850)
 select * from ARTICULOS
